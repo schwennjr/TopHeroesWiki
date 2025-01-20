9c95bbc3c6a2389fd3bf515f5b0b3068c02028b0
Craig Schwenn <schwennjr@gmail.com>
2025-01-19 18:17:20 -0600

    Fix path for dns name

--- build/assets/js/a7456010.e96164cc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktopheroes_wiki=self.webpackChunktopheroes_wiki||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
